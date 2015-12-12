7aedfe77d825e0ac45806bee3a125c8be3abf769
Alexei Zverev <alzverev@cisco.com>
2015-12-11 17:17:18 -0800

    Initializing of application

---
+++ js/shell.js
@@ -0,0 +1,58 @@
+(function(nx){
+
+	var app = new nx.ui.Application();
+
+	nx.define('TopologyContainer', nx.ui.Component, {
+		properties: {
+			topology: {
+				get: function () {
+					return this.view('topology');
+				}
+			}
+		},
+		view: {
+			content: [
+				{
+					'name': 'topology',
+					'type': 'nx.graphic.Topology',
+					'props': {
+						'adaptive': true,
+						'showIcon': true,
+						'nodeConfig': {
+							'label': 'model.name',
+							'iconType': 'switch',
+							'color': '#0how00'
+						},
+						'linkConfig': {
+							'linkType': 'curve'
+						},
+						nodeSetConfig: {
+							'label': 'model.label',
+							'iconType': 'groupL'
+						},
+						tooltipManagerConfig: {
+							nodeTooltipContentClass: 'TooltipNode',
+							linkTooltipContentClass: 'TooltipLink',
+							nodeSetTooltipContentClass: 'TooltipNode'
+						},
+						'identityKey': 'id',
+						'width': 800,
+						'height': 400,
+						'enableSmartLabel': true,
+						'enableGradualScaling': true,
+						'supportMultipleLink': true
+					}
+				}
+			]
+		}
+	});
+
+
+
+	var topologyContainer = new TopologyContainer();
+	var topo = topologyContainer.topology();
+	topo.attach(app);
+
+	app.container(document.getElementById('bier-topology'));
+
+})(nx);
